29ed1ae044bdc525014110c6f5363d159dec49ce
resur <sol.reshur@gmail.com>
2023-03-06 16:19:12 +0900

    06: False

---
+++ JS-100/problem/06.js
@@ -0,0 +1,16 @@
+/*
+# 문제6 : False
+
+다음은 자바스크립트 문법 중에서 False로 취급하는 것들 입니다.
+앗, False로 취급하지 않는 것이 하나 있네요! **True를 찾아주세요.**
+
+1)  NaN
+2)  1
+3)  ""
+4)  0
+5)  undefined
+
+출처 : https://obsidian-scene-3ac.notion.site/6-d1bd95ec2df4474385ab76c58f643ec1
+*/
+
+2;
