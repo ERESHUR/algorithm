d173ebf023e4e4511df75b3967ea2c0ee4f1a63f
resur <sol.reshur@gmail.com>
2023-03-06 16:16:42 +0900

    05: for문 계산

---
+++ JS-100/problem/05.js
@@ -0,0 +1,22 @@
+/*
+# 문제5 : for문 계산
+다음 코드의 출력 값으로 알맞은 것은?
+
+var a = 10;
+var b = 2;
+
+for(var i=1; i<5; i+=2){
+    a += i;
+}
+
+console.log(a+b);
+
+    1)  10
+    2)  12
+    3)  14
+    4)  16
+
+출처:https://obsidian-scene-3ac.notion.site/5-ce8ea9351c2f451b92ba1ec253239a98
+*/
+
+4;
