796614e5da08f9b29f09d75568bc6cd85dff94c8
resur <sol.reshur@gmail.com>
2023-03-06 16:25:54 +0900

    08: 객체의 키 이름 중복

---
+++ JS-100/problem/08.js
@@ -0,0 +1,20 @@
+/*
+# 문제8 : 객체의 키 이름 중복
+자바스크립트 객체를 다음과 같이 만들었다. 
+출력값을 입력하시오. (출력값은 공백을 넣지 않습니다. )
+
+    var d = {
+        'height':180,
+        'weight':78,
+        'weight':84,
+        'temperature':36,
+        'eyesight':1
+    };
+
+    console.log(d['weight']);
+
+출처: https://obsidian-scene-3ac.notion.site/8-14cb1fbe49354993ab22422a2a01be08
+*/
+
+84;
+// 맨 마지막에 입력된 데이터가 최종임
