d3d110dac84be904802ae9d194e9fd6cd20b3d9e
resur <sol.reshur@gmail.com>
2023-03-06 15:40:43 +0900

    01: 배열의 삭제

---
+++ JS-100/problem/01.js
@@ -0,0 +1,7 @@
+// # 문제1 : 배열의 삭제
+// 다음 배열에서 400, 500를 삭제하는 code를 입력하세요.
+// https://obsidian-scene-3ac.notion.site/1-46fb9eec72294ce0ab42c73a9feb7199
+var nums = [100, 200, 300, 400, 500];
+nums.pop();
+nums.pop();
+console.log(nums);
