d2352ce830fa3d697591d8550d9f7b2ba2e30b8f
resur <sol.reshur@gmail.com>
2023-03-06 16:21:14 +0900

    07: 변수명

---
+++ JS-100/problem/07.js
@@ -0,0 +1,14 @@
+/*
+# 문제7 : 변수명
+다음 중 변수명으로 사용할 수 없는 것 2개를 고르시오.
+
+    1)  age
+    2)  Age
+    3)  let
+    4)  _age
+    5)  1age
+
+출처: https://obsidian-scene-3ac.notion.site/7-ddbd1dc2b3ab47e2a405c29ec8425b2a
+*/
+3;
+5;
