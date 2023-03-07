724fc4e193e9419d450c9ff8a445f5b84bdb0253
resur <sol.reshur@gmail.com>
2023-03-07 12:32:24 +0900

    11: for를 이용한 기본 활용

---
+++ JS-100/problem/11.js
@@ -0,0 +1,19 @@
+/*
+# 문제11 : for를 이용한 기본 활용
+1부터 100까지 모두 더하는 Code를 <pass> 부분에 완성하세요. `for`를 사용해야 합니다.
+
+let s = 0;
+
+//pass
+
+console.log(s);
+
+출처: https://obsidian-scene-3ac.notion.site/11-for-e1aa3b5776fb4aa5b04addd81514f3a4
+*/
+
+let s = 0;
+
+for (let i = 0; i <= 100; i++) {
+  s = +i;
+}
+console.log(s);
